c710f956f5d227e22555d24be17d883bcf5210c9
samtapes <samtapes48@gmail.com>
2020-09-20 13:49:40 -0300

    some updates including returning if the user is admin

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,6 +1,8 @@
 const connection = require('../database/connection');
 
 module.exports = {
+
+    // Listing the users
     async index (req,res) {
         const users = await connection('users').select('*');
     
@@ -8,13 +10,27 @@ module.exports = {
     },
 
 
+    // Create session
     async create (req,res) {
+
+        // Getting the user data
         const { name, password } = req.body;
     
+
+        // If the name and the password are correct
         if(await connection('users').select('id').where('name', name).where('password', password).first()){
+
+            // Getting if the user is admin
+            const [ admin ]  = await connection('users').select('admin').where('name', name).where('password', password);
+
+            // Returning if the user is admin
+            res.header('X-Is-Admin', admin['admin']);
+
             return res.json({permission: "permited"})
         }
     
+
+        // if aren't
         else{
             return res.json({error: "Conta inexistente"})
         }
