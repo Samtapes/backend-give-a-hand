c38718ebe72d51988d6c7f47b7b2f60565056ab3
Samtapes <samtapes48@gmail.com>
2020-09-28 09:09:07 -0300

    an

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const configuration = require('../../knexfile');
 
 
-const env = process.env.NODE_ENV || 'staging'
-const connection = knex(configuration[env]);
+// const env = process.env.NODE_ENV || 'staging'
+const connection = knex(configuration.development);
 
 module.exports = connection;
