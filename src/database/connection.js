08e561cfebdb029b8b8da3141b73b94f02ac034b
Samtapes <samtapes48@gmail.com>
2020-09-28 10:47:37 -0300

    added pg

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-
 // const env = process.env.NODE_ENV || 'staging'
 const connection = knex(configuration.production);
 
